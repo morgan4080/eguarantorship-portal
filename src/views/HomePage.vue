1111ce223e312f7889fc587e70167478b1b59ab3
morgan4080 <morganmurungi@live.com>
2022-11-30 17:06:29 +0300

    typechek

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,135 +1,135 @@
 <script setup lang="ts">
-import * as d3 from 'd3';
-import Breadcrumb from "../components/Breadcrumb.vue";
-import {computed, ref, onMounted} from "vue";
-import stores from "../stores";
-const authStore = stores.authStore;
-const loanRequests = computed(() => authStore.getLoanRequests ? authStore.getLoanRequests : [
-  {
-    memberFirstName: 'MORGAN',
-    memberLastName: 'MUTUGI MURUNGI',
-    loanAmount: 500000,
-    loanProductName: 'Inua Loan'
-  },
-  {
-    memberFirstName: 'MORGAN',
-    memberLastName: 'MUTUGI MURUNGI',
-    loanAmount: 500000,
-    loanProductName: 'Inua Loan'
-  },
-  {
-    memberFirstName: 'MORGAN',
-    memberLastName: 'MUTUGI MURUNGI',
-    loanAmount: 500000,
-    loanProductName: 'Inua Loan'
-  },
-  {
-    memberFirstName: 'MORGAN',
-    memberLastName: 'MUTUGI MURUNGI',
-    loanAmount: 500000,
-    loanProductName: 'Inua Loan'
-  },
-  {
-    memberFirstName: 'MORGAN',
-    memberLastName: 'MUTUGI MURUNGI',
-    loanAmount: 500000,
-    loanProductName: 'Inua Loan'
-  },
-  {
-    memberFirstName: 'MORGAN',
-    memberLastName: 'MUTUGI MURUNGI',
-    loanAmount: 500000,
-    loanProductName: 'Inua Loan'
-  }
-]);
-const hrWidth = (step: number = 0) => {
-  return (step * 100) / 3
-};
+  import * as d3 from 'd3';
+  import Breadcrumb from "../components/Breadcrumb.vue";
+  import {computed, ref, onMounted} from "vue";
+  import stores from "../stores";
+  const {loanRequestStore} = stores;
+  const loanRequests = computed(() => loanRequestStore.getLoanRequests ? loanRequestStore.getLoanRequests : [
+    {
+      memberFirstName: 'MORGAN',
+      memberLastName: 'MUTUGI MURUNGI',
+      loanAmount: 500000,
+      loanProductName: 'Inua Loan'
+    },
+    {
+      memberFirstName: 'MORGAN',
+      memberLastName: 'MUTUGI MURUNGI',
+      loanAmount: 500000,
+      loanProductName: 'Inua Loan'
+    },
+    {
+      memberFirstName: 'MORGAN',
+      memberLastName: 'MUTUGI MURUNGI',
+      loanAmount: 500000,
+      loanProductName: 'Inua Loan'
+    },
+    {
+      memberFirstName: 'MORGAN',
+      memberLastName: 'MUTUGI MURUNGI',
+      loanAmount: 500000,
+      loanProductName: 'Inua Loan'
+    },
+    {
+      memberFirstName: 'MORGAN',
+      memberLastName: 'MUTUGI MURUNGI',
+      loanAmount: 500000,
+      loanProductName: 'Inua Loan'
+    },
+    {
+      memberFirstName: 'MORGAN',
+      memberLastName: 'MUTUGI MURUNGI',
+      loanAmount: 500000,
+      loanProductName: 'Inua Loan'
+    }
+  ]);
+  const hrWidth = (step: number = 0) => {
+    return (step * 100) / 3
+  };
 
-const filter = ref('Past Week')
+  const filter = ref('Past Week')
 
-const data = [2,4,6,8,10,14,20]
+  const data = [2,4,6,8,10,14,20]
 
-const targetD3 = ref(null)
+  const targetD3 = ref(null)
 
-const targetD30 = ref(null)
+  const targetD30 = ref(null)
 
-onMounted(() => {
-  if (targetD3.value) {
-    (() => {
-      let svg = d3.select(targetD3.value)
-      let width: any = svg.attr('width')
-      let height: any = svg.attr('height')
-      let radius = Math.min(width, height) / 2
-      let g = svg.append('g').attr('transform', "translate(" + width / 2 + "," + height / 2 +")")
-      // create ordinal scale
-      let color = d3.scaleOrdinal(['#e40303', '#ff8c00', '#008026','#004dff', '#750787']);
-      // create pie
-      let pie = d3.pie()
-      // create arc
-      let arc: any = d3.arc()
-          .innerRadius(radius/2)
-          .outerRadius(radius)
-      // create arcs
-      let arcs = g.selectAll('arc')
-          .data(pie(data))
-          .enter().append('g')
-          .attr('class', 'arc')
-      // append path
-      arcs.append('path')
-          .attr('fill', (d, i: any) => color(i))
-          .attr('d', arc);
-      let textContainer = g.append('g');
-      let text = textContainer.append('text');
-      text.attr("transform", function() {
-        let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
-        return "translate(" + centroid[0] + "," + 0 + ")";
-      }).attr('class', 'text-sm fill-eg-bg').text('Total');
-      let text2 = textContainer.append('text');
-      text2.attr("transform", function() {
-        let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
-        return "translate(" + centroid[0] + "," + 15 + ")";
-      }).attr('class', 'text-xxs font-semibold fill-red-500').text('KES 32M');
-    })()
-  }
-  if (targetD30.value) {
-    (() => {
-      let svg = d3.select(targetD30.value)
-      let width: any = svg.attr('width')
-      let height: any = svg.attr('height')
-      let radius = Math.min(width, height) / 2
-      let g = svg.append('g').attr('transform', "translate(" + width / 2 + "," + height / 2 +")")
-      // create ordinal scale
-      let color = d3.scaleOrdinal(['#e40303', '#ff8c00', '#008026','#004dff', '#750787']);
-      // create pie
-      let pie = d3.pie()
-      // create arc
-      let arc: any = d3.arc()
-          .innerRadius(radius/2)
-          .outerRadius(radius)
-      // create arcs
-      let arcs = g.selectAll('arc')
-          .data(pie(data))
-          .enter().append('g')
-          .attr('class', 'arc')
-      // append path
-      arcs.append('path')
-          .attr('fill', (d, i: any) => color(i))
-          .attr('d', arc);
-      let textContainer = g.append('g');
-      let text = textContainer.append('text');
-      text.attr("transform", function() {
-        let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
-        return "translate(" + centroid[0] + "," + 0 + ")";
-      }).attr('class', 'text-sm fill-eg-bg').text('Total');
-      let text2 = textContainer.append('text');
-      text2.attr("transform", function() {
-        let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
-        return "translate(" + centroid[0] + "," + 15 + ")";
-      }).attr('class', 'text-xxs font-semibold fill-red-500').text('KES 20M');
-    })()
-  }
-})
+  onMounted(() => {
+    if (targetD3.value) {
+      (() => {
+        let svg = d3.select(targetD3.value)
+        let width: any = svg.attr('width')
+        let height: any = svg.attr('height')
+        let radius = Math.min(width, height) / 2
+        let g = svg.append('g').attr('transform', "translate(" + width / 2 + "," + height / 2 +")")
+        // create ordinal scale
+        let color = d3.scaleOrdinal(['#e40303', '#ff8c00', '#008026','#004dff', '#750787']);
+        // create pie
+        let pie = d3.pie()
+        // create arc
+        let arc: any = d3.arc()
+            .innerRadius(radius/2)
+            .outerRadius(radius)
+        // create arcs
+        let arcs = g.selectAll('arc')
+            .data(pie(data))
+            .enter().append('g')
+            .attr('class', 'arc')
+        // append path
+        arcs.append('path')
+            .attr('fill', (d, i: any) => color(i))
+            .attr('d', arc);
+        let textContainer = g.append('g');
+        let text = textContainer.append('text');
+        text.attr("transform", function() {
+          let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
+          return "translate(" + centroid[0] + "," + 0 + ")";
+        }).attr('class', 'text-sm fill-eg-bg').text('Total');
+        let text2 = textContainer.append('text');
+        text2.attr("transform", function() {
+          let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
+          return "translate(" + centroid[0] + "," + 15 + ")";
+        }).attr('class', 'text-xxs font-semibold fill-red-500').text('KES 32M');
+      })()
+    }
+    if (targetD30.value) {
+      (() => {
+        let svg = d3.select(targetD30.value)
+        let width: any = svg.attr('width')
+        let height: any = svg.attr('height')
+        let radius = Math.min(width, height) / 2
+        let g = svg.append('g').attr('transform', "translate(" + width / 2 + "," + height / 2 +")")
+        // create ordinal scale
+        let color = d3.scaleOrdinal(['#e40303', '#ff8c00', '#008026','#004dff', '#750787']);
+        // create pie
+        let pie = d3.pie()
+        // create arc
+        let arc: any = d3.arc()
+            .innerRadius(radius/2)
+            .outerRadius(radius)
+        // create arcs
+        let arcs = g.selectAll('arc')
+            .data(pie(data))
+            .enter().append('g')
+            .attr('class', 'arc')
+        // append path
+        arcs.append('path')
+            .attr('fill', (d, i: any) => color(i))
+            .attr('d', arc);
+        let textContainer = g.append('g');
+        let text = textContainer.append('text');
+        text.attr("transform", function() {
+          let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
+          return "translate(" + centroid[0] + "," + 0 + ")";
+        }).attr('class', 'text-sm fill-eg-bg').text('Total');
+        let text2 = textContainer.append('text');
+        text2.attr("transform", function() {
+          let centroid = arc.centroid({startAngle: ((radius/2)+radius)/2, endAngle: ((radius/2)+radius)/2})
+          return "translate(" + centroid[0] + "," + 15 + ")";
+        }).attr('class', 'text-xxs font-semibold fill-red-500').text('KES 20M');
+      })()
+    }
+  })
 
 </script>
 <template>
