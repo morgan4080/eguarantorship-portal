90065c8a63f5563b0686500b93ebb962b60881b3
dkimani <mdkimani@gmail.com>
2023-01-20 15:39:25 +0300

    changed name to eguarantorship ui

--- docker-build-ecr.sh
+++ docker-build-ecr.sh
@@ -1,4 +1,4 @@
-echo '===> Compiling & Packaging prestaapps/presta-eguarantorship ...'
+echo '===> Compiling & Packaging prestaapps/eguarantorshipui ...'
 npm install
 npm run deploy
 
@@ -15,19 +15,19 @@ fi
 
 echo "APP VERSION = "$APP_VERSION
 
-echo '===> Building prestaapps/presta-eguarantorship docker image...'
+echo '===> Building prestaapps/eguarantorshipui docker image...'
 #Oauth service
 docker build \
   -q \
-  -t 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/prestaeguarantorship:latest \
-  -t 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/prestaeguarantorship:"$APP_VERSION" \
+  -t 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/eguarantorshipui:latest \
+  -t 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/eguarantorshipui:"$APP_VERSION" \
   --build-arg GIT_BRANCH="$GIT_BRANCH" \
   --build-arg GIT_COMMIT="$GIT_COMMIT" \
   --build-arg GIT_DIRTY="$GIT_DIRTY" \
   --build-arg BUILD_CREATOR="$BUILD_CREATOR" \
   --build-arg BUILD_NUMBER="$BUILD_NUMBER" \
   .
-echo "Done building prestaapps/prestaeguarantorship:"$APP_VERSION
+echo "Done building prestaapps/eguarantorshipui:"$APP_VERSION
 
-docker push 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/prestaeguarantorship:"$APP_VERSION"
-docker push 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/prestaeguarantorship:latest
+docker push 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/eguarantorshipui:"$APP_VERSION"
+docker push 665804139994.dkr.ecr.us-west-2.amazonaws.com/prestaapps/eguarantorshipui:latest
